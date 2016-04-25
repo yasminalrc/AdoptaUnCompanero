bc062301cc973a1e0b971d2a5cb910add8f5183f
urbanz <guillermourbano@MacBook-Pro-de-Guillermo.local>
2016-04-25 12:00:07 +0200

    Estructura angular
    Archivos de configuración

---
+++ Angular/src/app/mascota.ts
@@ -0,0 +1,19 @@
+export class Mascota {
+
+  especie: string;
+  tamaño: string;
+  raza: string;
+  edad: string;
+  sexo: string;
+  fecha: string;
+
+  constructor (especie: string, tamaño: string, raza: string, edad: string, sexo: string, fecha: string){
+    this.especie = especie;
+    this.tamaño = tamaño;
+    this.raza = raza;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.fecha = fecha;
+  }
+
+}
